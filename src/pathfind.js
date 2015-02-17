f5dbdd43fecf9c47559e94872866e56ad64e8619
rfmx49 <rfmx49@gmail.com>
2015-02-17 13:57:20 -0500

    W3_1
    Path finding changes.

--- src/pathfind.js
+++ src/pathfind.js
@@ -3,11 +3,11 @@ function pathFind(id, x, y) {
 	var needToMove = false;
 	var loopOverfill = 0;
 	var endPos = {x:(x+playerOffset),y:(y+playerOffset)}; //add playerOffset to center the player on the tile.
-	endPos.xtile = (x/_tileSize);
-	endPos.ytile = (y/_tileSize);
-	//toConsole(endPos);
+	endPos.xtile = Math.floor((x)/_tileSize);
+	endPos.ytile = Math.floor((y)/_tileSize);
+	console.log(endPos);
 	//get current pos.
-	var currentPos = {x: (Crafty(Crafty('PlayerCharacter')[0])._x),y:(Crafty(Crafty('PlayerCharacter')[0])._y),rotation:(Crafty(Crafty('PlayerCharacter')[0])._rotation),type:"f"};
+	var currentPos = {x: (Crafty(id)._x),y:(Crafty(id)._y),rotation:(Crafty(id)._rotation),type:"f"};
 	var revertPos = {};
 	//set player in motion.
 	var findingPath = true;
@@ -19,27 +19,30 @@ function pathFind(id, x, y) {
 		currentPos.ytile = Math.floor((currentPos.y)/_tileSize);
 		currentPos.type = "f";
 		revertPos = currentPos;
-		//toConsole("move player(" + id + ") from x: " + currentPos.x + " y: (tile[" + currentPos.xtile + "][" + currentPos.ytile + "])" + currentPos.y + " TO x:" + endPos.x + "y:" + endPos.y + "(tile[" + endPos.xtile + "][" + endPos.ytile + "])");
+		console.log("move player(" + id + ") from x: " + currentPos.x + " y: " + currentPos.y + " (tile[" + currentPos.xtile + "][" + currentPos.ytile + "])" + " TO x:" + endPos.x + "y:" + endPos.y + "(tile[" + endPos.xtile + "][" + endPos.ytile + "])");
 		needToMove = false;
 		decider = Math.floor(Math.random() * 2) + 1;
 		//1 = moving along x
 		//2 = moving along y
 		if (decider == 1) {
-			//toConsole("moving along x");
+			console.log("moving along x");
 			//check direction to move negitive(left)
 			//positive(right)
 			if (currentPos.x == endPos.x) {
 				//no need to move.
-				//toConsole("no need to move on same x");
+				console.log("no need to move on same x");
 				needToMove = false;
 			}
 			else if (currentPos.x < endPos.x) {
 				//move right ++
 				//check floor tile to right
-				if ((floorMap[currentPos.ytile][(currentPos.xtile + 1)] == "f") || (floorMap[currentPos.ytile][(currentPos.xtile + 1)].substring(0,1) == "d")) {
+				if ((floorMap[currentPos.ytile][(currentPos.xtile + 1)] == "f") || (floorMap[currentPos.ytile][(currentPos.xtile + 1)].substring(0,1) == "d") && (endPos.ytile == currentPos.ytile)) {
 					//move player
 					needToMove = true;
-					currentPos.x = currentPos.x + _tileSize;
+					console.log("current pos: " + currentPos.x);
+					currentPos.x = Crafty('Tile' + currentPos.ytile + '_' + (currentPos.xtile + 1))._x;
+					currentPos.y = Crafty('Tile' + currentPos.ytile + '_' + (currentPos.xtile + 1))._y;
+					console.log("current pos: " + currentPos.x);
 					currentPos.rotation = 90;
 					if (floorMap[currentPos.ytile][(currentPos.xtile + 1)].substring(0,1) == "d"){
 						if (!((currentPos.x == endPos.x) && (currentPos.y == endPos.y))) {
@@ -50,17 +53,20 @@ function pathFind(id, x, y) {
 					}					
 				}
 				else {
-					//toConsole("no tile is not a floor" + floorMap[currentPos.ytile][(currentPos.xtile + 1)]);
+					console.log("no tile is not a floor" + floorMap[currentPos.ytile][(currentPos.xtile + 1)]);
 					//tile to right is not a floor dont move override next decider.
 				}
 			}
 			else if (currentPos.x > endPos.x) {
 				//move left --
 				//check floor tile to left
-				if ((floorMap[currentPos.ytile][(currentPos.xtile - 1)] == "f") || (floorMap[currentPos.ytile][(currentPos.xtile - 1)].substring(0,1) == "d")){
+				if ((floorMap[currentPos.ytile][(currentPos.xtile - 1)] == "f") || (floorMap[currentPos.ytile][(currentPos.xtile - 1)].substring(0,1) == "d") && (endPos.ytile == currentPos.ytile)){
 					//move player
 					needToMove = true;
-					currentPos.x = currentPos.x - _tileSize;
+					console.log("current pos: " + currentPos.x);
+					currentPos.x = Crafty('Tile' + currentPos.ytile + '_' + (currentPos.xtile - 1))._x;
+					currentPos.y = Crafty('Tile' + currentPos.ytile + '_' + (currentPos.xtile - 1))._y;
+					console.log("current pos: " + currentPos.x);
 					if (currentPos.rotation == 0) {
 						currentPos.rotation = -90;
 					}
@@ -75,29 +81,30 @@ function pathFind(id, x, y) {
 				}
 				else {
 					//tile to right is not a floor dont move override next decider.
-					//toConsole("no tile is not a floor" + floorMap[currentPos.ytile][(currentPos.xtile - 1)]);
+					console.log("no tile is not a floor" + floorMap[currentPos.ytile][(currentPos.xtile - 1)]);
 				}
 			}
 			else {
-				//toConsole("ERROR not equal or less or greater" + currentPos.x + " " + endPos.x);
+				console.log("ERROR not equal or less or greater" + currentPos.x + " " + endPos.x);
 			}
 		}
 		else {
-			//toConsole("moving along y");
+			console.log("moving along y");
 			//check direction to move negitive(up)
 			//positive(down)
 			if (currentPos.y == endPos.y) {
 				//no need to move.
-				//toConsole("no need to move on same y");
+				console.log("no need to move on same y");
 				needToMove = false;
 			}
 			else if (currentPos.y < endPos.y) {
 				//move down ++
 				//check floor tile below
-				if ((floorMap[(currentPos.ytile + 1)][currentPos.xtile] == "f") || (floorMap[(currentPos.ytile + 1)][currentPos.xtile].substring(0,1) == "d")) {
+				if ((floorMap[(currentPos.ytile + 1)][currentPos.xtile] == "f") || (floorMap[(currentPos.ytile + 1)][currentPos.xtile].substring(0,1) == "d")  && (endPos.xtile == currentPos.xtile)) {
 					//move player
 					needToMove = true;
-					currentPos.y = currentPos.y + _tileSize;
+					currentPos.x = Crafty('Tile' + (currentPos.ytile+1) + '_' + (currentPos.xtile))._x;
+					currentPos.y = Crafty('Tile' + (currentPos.ytile+1) + '_' + (currentPos.xtile))._y;
 					currentPos.rotation = 180;		
 					if (floorMap[(currentPos.ytile + 1)][currentPos.xtile].substring(0,1) == "d"){
 						if (!((currentPos.x == endPos.x) && (currentPos.y == endPos.y))) {
@@ -109,17 +116,18 @@ function pathFind(id, x, y) {
 				}
 				else {
 					//tile to right is not a floor dont move override next decider.
-					//toConsole("no tile is not a floor" + floorMap[(currentPos.ytile + 1)][currentPos.xtile]);
+					console.log("no tile is not a floor" + floorMap[(currentPos.ytile + 1)][currentPos.xtile]);
 					needToMove = false;
 				}
 			}
 			else if (currentPos.y > endPos.y) {
 				//move up --
 				//check floor tile above
-				if ((floorMap[(currentPos.ytile - 1)][currentPos.xtile] == "f") || (floorMap[(currentPos.ytile - 1)][currentPos.xtile].substring(0,1) == "d")) {
+				if ((floorMap[(currentPos.ytile - 1)][currentPos.xtile] == "f") || (floorMap[(currentPos.ytile - 1)][currentPos.xtile].substring(0,1) == "d") && (endPos.xtile == currentPos.xtile)) {
 					//move player
 					needToMove = true;
-					currentPos.y = currentPos.y - _tileSize;
+					currentPos.x = Crafty('Tile' + (currentPos.ytile-1) + '_' + (currentPos.xtile))._x;
+					currentPos.y = Crafty('Tile' + (currentPos.ytile-1) + '_' + (currentPos.xtile))._y;
 					if (currentPos.rotation == 270) {
 						currentPos.rotation = 360;
 					}
@@ -134,17 +142,17 @@ function pathFind(id, x, y) {
 				}
 				else {
 					needToMove = false;
-					//toConsole("no tile is not a floor" + floorMap[(currentPos.ytile - 1)][currentPos.xtile]);
+					console.log("no tile is not a floor" + floorMap[(currentPos.ytile - 1)][currentPos.xtile]);
 					//tile to right is not a floor dont move override next decider.
 				}
 			}
 			else {
-				//toConsole("ERROR not equal or less or greater" + currentPos.y + " " + endPos.y);
+				console.log("ERROR not equal or less or greater" + currentPos.y + " " + endPos.y);
 			}
 		}
 		//do we need to move player this loop?
 		if (needToMove) {
-			//toConsole("yes need to move player(" + id + ") to " + currentPos + " Rotation:" + currentPos.rotation);
+			console.log("yes need to move player(" + id + ") to " + currentPos + " Rotation:" + currentPos.rotation);
 			Crafty(id).nodePath.push({x:currentPos.x,y:currentPos.y,rotation:currentPos.rotation,type:currentPos.type});
 			if (currentPos.rotation == 360) {
 				currentPos.rotation = 0;
@@ -154,23 +162,23 @@ function pathFind(id, x, y) {
 			}				
 		}
 		else {
-			//toConsole("do not need to move");
+			console.log("do not need to move");
 		}
 		if ((currentPos.x == endPos.x) && (currentPos.y == endPos.y)) {
 			//play tween file.
 			findingPath = false;
 		}
 		else { 
-			//toConsole(currentPos.x + " " + endPos.x + " " + currentPos.y + " " + endPos.y); 
+			console.log("no ending " + currentPos.x + " " + endPos.x + " " + currentPos.y + " " + endPos.y); 
 		}
 		//check if loop is getting out of control.
-		if (loopOverfill > 500) {
+		if (loopOverfill > 100) {
 			Crafty(id).nodePath = [];
 			findingPath = false;
 		}
 	}
 	if (Crafty(id).nodePath.length > 0) {
-		//toConsole(Crafty(id).nodePath);
+		console.log(Crafty(id).nodePath);
 		Crafty(id).playTween();
 	}	
 };
