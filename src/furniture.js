bd8f0ddb05f9ff3a16df7db01654b900b4acc455
rfmx49 <rfmx49@gmail.com>
2015-09-10 15:40:44 -0400

    Added 2x2 furniture and better more logical furniture placement

--- src/furniture.js
+++ src/furniture.js
@@ -51,7 +51,7 @@ function placeFurniture() {
 	var x;
 	var offsetPlacement;
 
-	var openPostions = [];
+	var openPositions = [];
 	//fix centering
 
 	for (var row = 0; row < rows; row++) {
@@ -63,22 +63,22 @@ function placeFurniture() {
 				//check below it/diagonal to it
 				//Check all possible placements.
 				//N above
-				openPostions = [];
+				openPositions = [];
 				if (!(typeof objectMap[row-1] === 'undefined')) {
 					//N above
 					if (objectMap[row-1][col] == "o") {
-						openPostions.push('N');
+						openPositions.push('N');
 					}
 					//NW
 					if (!(typeof objectMap[row-1][col-1] === 'undefined')) {	
 						if (objectMap[row-1][col-1] == "o") {
-							openPostions.push('NW');
+							openPositions.push('NW');
 						}
 					}	
 					//NE
 					if (!(typeof objectMap[row-1][col+1] === 'undefined')) {	
 						if (objectMap[row-1][col+1] == "o") {
-							openPostions.push('NE');
+							openPositions.push('NE');
 						}
 					}	
 				}
@@ -86,31 +86,31 @@ function placeFurniture() {
 				if (!(typeof objectMap[row+1] === 'undefined')) {
 					//S above
 					if (objectMap[row+1][col] == "o") {
-						openPostions.push('S');
+						openPositions.push('S');
 					}
 					//SW
 					if (!(typeof objectMap[row+1][col-1] === 'undefined')) {	
 						if (objectMap[row+1][col-1] == "o") {
-							openPostions.push('SW');
+							openPositions.push('SW');
 						}
 					}	
 					//SE
 					if (!(typeof objectMap[row+1][col+1] === 'undefined')) {	
 						if (objectMap[row+1][col+1] == "o") {
-							openPostions.push('SE');
+							openPositions.push('SE');
 						}
 					}
 				}
 				//E
 				if (!(typeof objectMap[row][col+1] === 'undefined')) {	
 					if (objectMap[row][col+1] == "o") {
-						openPostions.push('E');
+						openPositions.push('E');
 					}
 				}
 				//W
 				if (!(typeof objectMap[row][col-1] === 'undefined')) {	
 					if (objectMap[row][col-1] == "o") {
-						openPostions.push('W');
+						openPositions.push('W');
 					}
 				}
 
@@ -133,33 +133,75 @@ function placeFurniture() {
 						Crafty.e('TileOpen' + row + '_' + col + ', furnitureMap , 1_' + roomColour + '_1x1_' + furnitureVariant)
 							.attr({y: Crafty('Tile' + row + '_' + col)._y, x: Crafty('Tile' + row + '_' + col)._x, w: _tileSize, h: _tileSize, xTile: col, yTile: row});
 						Crafty(Crafty('FloorGround')[0]).attach(Crafty('TileOpen' + row + '_' + col));
+						Crafty('TileOpen' + row + '_' + col).origin('center');
 						objectMap[row][col] = "T";
+						//better rotating
+						var betterRotation = 0;
+						//pop out all ordial directions
+						x = openPositions.indexOf("NE");
+						if (x != -1) { openPositions.splice(x,1); }
+						x = openPositions.indexOf("SE");
+						if (x != -1) { openPositions.splice(x,1); }
+						x = openPositions.indexOf("NW");
+						if (x != -1) { openPositions.splice(x,1); }
+						x = openPositions.indexOf("SW");
+						if (x != -1) { openPositions.splice(x,1); }
+						//want to put it into non open facing postion.
+						if ((floorMap[row-1][col].substring(0,1) == "w")) { betterRotation = 0; }
+						else {
+							if ((floorMap[row+1][col].substring(0,1) == "w")) { betterRotation = 180; }
+							else {
+								if ((floorMap[row][col+1].substring(0,1) == "w")) { betterRotation = 90; }
+								else {
+									if ((floorMap[row][col-1].substring(0,1) == "w")) { betterRotation = 270; }
+									else{
+										x = openPositions.indexOf("N");
+										if (x == -1) { betterRotation = 0; }
+										else {
+											x = openPositions.indexOf("S");
+											if (x == -1) { betterRotation = 180; }
+											else {
+												x = openPositions.indexOf("E");
+												if (x == -1) { betterRotation = 90; }
+												else {
+													x = openPositions.indexOf("W");
+													if (x == -1) { betterRotation = 270; }
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						Crafty('TileOpen' + row + '_' + col).rotation = betterRotation;
+						
 					}
 					else if (roomVariant == 2) {
 						placementFound = true;
+						var dualPositions = openPositions.slice();
 						//check will this fit.
 						//pop out all ordial directions
-						x = openPostions.indexOf("NE");
-						if (x != -1) { openPostions.splice(x,1); }
-						x = openPostions.indexOf("SE");
-						if (x != -1) { openPostions.splice(x,1); }
-						x = openPostions.indexOf("NW");
-						if (x != -1) { openPostions.splice(x,1); }
-						x = openPostions.indexOf("SW");
-						if (x != -1) { openPostions.splice(x,1); }
+						x = dualPositions.indexOf("NE");
+						if (x != -1) { dualPositions.splice(x,1); }
+						x = dualPositions.indexOf("SE");
+						if (x != -1) { dualPositions.splice(x,1); }
+						x = dualPositions.indexOf("NW");
+						if (x != -1) { dualPositions.splice(x,1); }
+						x = dualPositions.indexOf("SW");
+						if (x != -1) { dualPositions.splice(x,1); }
 
-						//check the length of x = openPostions if it is 1 then that is the direcction other wise use random to find.
-						if (openPostions.length == 0) {
+						//check the length of x = openPositions if it is 1 then that is the direcction other wise use random to find.
+						if (dualPositions.length == 0) {
 							placementFound = false;
 						}
-						else if (openPostions.length == 1) {
+						else if (dualPositions.length == 1) {
 							//get rotation going N/S or E/W (default is E/W)
-							furnitureRotation = openPostions[x]
+							furnitureRotation = dualPositions[0]
 						}
 						else {
 							//pick randomly
-							x = Math.floor(furnitureRandom() * openPostions.length)
-							furnitureRotation = openPostions[x]
+							x = Math.floor(furnitureRandom() * dualPositions.length)
+							furnitureRotation = dualPositions[x]
 						}
 
 						if (furnitureRotation != "") {
@@ -168,11 +210,60 @@ function placeFurniture() {
 								Crafty.e('TileOpen' + row + '_' + col + ', furnitureMap , 1_' + roomColour + '_1x2_' + furnitureVariant)
 									.attr({y: Crafty('Tile' + row + '_' + col)._y, x: Crafty('Tile' + row + '_' + col)._x, w: _tileSize*2, h: _tileSize, xTile: col, yTile: row});
 								Crafty(Crafty('FloorGround')[0]).attach(Crafty('TileOpen' + row + '_' + col));
+								Crafty('TileOpen' + row + '_' + col).origin("center");
 								//block off tiles
 								objectMap[row][col] = "T";
 								objectMap[row][col+1] = "T";
 								floorMap[row][col+1] = "o";
-								
+
+								//better Rotation
+								var betterRotation = 0;
+								if ((floorMap[row-1][col].substring(0,1) == "w")) { betterRotation = 0; }
+								else {
+									if ((floorMap[row+1][col].substring(0,1) == "w")) { betterRotation = 180; }
+									else {
+										x = openPositions.indexOf("N");
+										if (x == -1) { betterRotation = 0; }
+										else {
+											x = openPositions.indexOf("S");
+											if (x == -1) { betterRotation = 180; }
+										}
+									}
+								}
+								Crafty('TileOpen' + row + '_' + col).rotation = betterRotation;
+								//set tiles infront to be open if notheing there already
+								if (betterRotation == 0) {
+									//South should be left open
+									if (!(typeof objectMap[row+1][col] === 'undefined')) {	
+										if (objectMap[row+1][col] == "o") {
+											objectMap[row+1][col] == "FR";
+											floorMap[row+1][col] == "FR";
+										}
+									}	
+									if (!(typeof objectMap[row+1][col+1] === 'undefined')) {	
+										if (objectMap[row+1][col+1] == "o") {
+											objectMap[row+1][col+1] == "FR";
+											floorMap[row+1][col+1] == "FR";
+										}
+									}
+									
+								}
+								else if (betterRotation == 180) {
+									//North should be left open
+									if (!(typeof objectMap[row+1][col] === 'undefined')) {	
+										if (objectMap[row-1][col] == "o") {
+											objectMap[row-1][col] == "FR";
+											floorMap[row-1][col] == "FR";
+										}
+									}	
+									if (!(typeof objectMap[row+1][col+1] === 'undefined')) {	
+										if (objectMap[row-1][col+1] == "o") {
+											objectMap[row-1][col+1] == "FR";
+											floorMap[row-1][col+1] == "FR";
+										}
+									}
+								}
+																
 							}
 							else if (furnitureRotation == "W") {
 								furnitureVariant = Math.floor(furnitureRandom() * furniture1x2Variants) + 1
@@ -180,10 +271,59 @@ function placeFurniture() {
 									.attr({y: Crafty('Tile' + row + '_' + col)._y, x: Crafty('Tile' + row + '_' + (col-1))._x, w: _tileSize*2, h: _tileSize, xTile: col, yTile: row});
 								Crafty(Crafty('FloorGround')[0]).attach(Crafty('TileOpen' + row + '_' + col));
 								Crafty('TileOpen' + row + '_' + col).origin("center");
-								Crafty('TileOpen' + row + '_' + col).rotation = 180;
 								objectMap[row][col] = "T";
 								objectMap[row][col-1] = "T";
 								floorMap[row][col-1] = "o";
+
+								//better Rotation
+								var betterRotation = 180;
+								if ((floorMap[row-1][col].substring(0,1) == "w")) { betterRotation = 0; }
+								else {
+									if ((floorMap[row+1][col].substring(0,1) == "w")) { betterRotation = 180; }
+									else {
+										x = openPositions.indexOf("N");
+										if (x == -1) { betterRotation = 0; }
+										else {
+											x = openPositions.indexOf("S");
+											if (x == -1) { betterRotation = 180; }
+										}
+									}
+								}
+								Crafty('TileOpen' + row + '_' + col).rotation = betterRotation;
+
+								//set tiles infront to be open if notheing there already
+								if (betterRotation == 0) {
+									//South should be left open
+									if (!(typeof objectMap[row+1][col] === 'undefined')) {	
+										if (objectMap[row+1][col] == "o") {
+											objectMap[row+1][col] == "FR";
+											floorMap[row+1][col] == "FR";
+										}
+									}	
+									if (!(typeof objectMap[row+1][col+1] === 'undefined')) {	
+										if (objectMap[row+1][col-1] == "o") {
+											objectMap[row+1][col-1] == "FR";
+											floorMap[row+1][col-1] == "FR";
+										}
+									}
+									
+								}
+								else if (betterRotation == 180) {
+									//North should be left open
+									if (!(typeof objectMap[row+1][col] === 'undefined')) {	
+										if (objectMap[row-1][col] == "o") {
+											objectMap[row-1][col] == "FR";
+											floorMap[row-1][col] == "FR";
+										}
+									}	
+									if (!(typeof objectMap[row+1][col+1] === 'undefined')) {	
+										if (objectMap[row-1][col-1] == "o") {
+											objectMap[row-1][col-1] == "FR";
+											floorMap[row-1][col-1] == "FR";
+										}
+									}
+								}
+								
 							}
 							else if (furnitureRotation == "N") {
 
@@ -192,11 +332,27 @@ function placeFurniture() {
 									.attr({y: Crafty('Tile' + row + '_' + col)._y, x: Crafty('Tile' + row + '_' + col)._x, w: _tileSize*2, h: _tileSize, xTile: col, yTile: row});
 								Crafty(Crafty('FloorGround')[0]).attach(Crafty('TileOpen' + row + '_' + col));
 								Crafty('TileOpen' + row + '_' + col).origin(_tileSize/2,_tileSize/2);
-								Crafty('TileOpen' + row + '_' + col).rotation = 270;
 								//block off tiles
 								objectMap[row][col] = "T";
 								objectMap[row-1][col] = "T";
 								floorMap[row-1][col] = "o";
+								Crafty('TileOpen' + row + '_' + col).rotation = 270;
+
+								//better Rotation
+								var betterRotation = 270;
+								if ((floorMap[row][col+1].substring(0,1) == "w")) { betterRotation = 270; }
+								else {
+									if ((floorMap[row][col-1].substring(0,1) == "w")) { betterRotation = 90; }
+									else {
+										x = openPositions.indexOf("E");
+										if (x == -1) { betterRotation = 270; }
+										else {
+											x = openPositions.indexOf("W");
+											if (x == -1) { betterRotation = 90; }
+										}
+									}
+								}
+								if (betterRotation == 270) { Crafty('TileOpen' + row + '_' + col).flip('Y'); }
 
 							}
 							else if (furnitureRotation == "S") {
@@ -205,13 +361,130 @@ function placeFurniture() {
 									.attr({y: Crafty('Tile' + row + '_' + col)._y, x: Crafty('Tile' + row + '_' + col)._x, w: _tileSize*2, h: _tileSize, xTile: col, yTile: row});
 								Crafty(Crafty('FloorGround')[0]).attach(Crafty('TileOpen' + row + '_' + col));
 								Crafty('TileOpen' + row + '_' + col).origin(_tileSize/2,_tileSize/2);
-								Crafty('TileOpen' + row + '_' + col).rotation = 90;
 								//block off tiles
+								Crafty('TileOpen' + row + '_' + col).rotation = 90;
 								objectMap[row][col] = "T";
 								objectMap[row+1][col] = "T";
 								floorMap[row+1][col] = "o";
+
+								//better Rotation
+								var betterRotation = 90;
+								if ((floorMap[row][col+1].substring(0,1) == "w")) { betterRotation = 270; }
+								else {
+									if ((floorMap[row][col-1].substring(0,1) == "w")) { betterRotation = 90; }
+									else {
+										x = openPositions.indexOf("E");
+										if (x == -1) { betterRotation = 270; }
+										else {
+											x = openPositions.indexOf("W");
+											if (x == -1) { betterRotation = 90; }
+										}
+									}
+								}
+								if (betterRotation == 90) { Crafty('TileOpen' + row + '_' + col).flip('Y'); }
 							}
 						}
+					}
+					else if (roomVariant == 3) {
+						placementFound = true;
+						//check 4 postions to see if they are open
+						var quadsAvailible = [];
+						//NW
+						if ((openPositions.indexOf("NW") != -1 && openPositions.indexOf("W") != -1 && openPositions.indexOf("N") != -1)){
+							quadsAvailible.push('NW'); 
+						}
+						//NE
+						if ((openPositions.indexOf("NE") != -1 && openPositions.indexOf("E") != -1 && openPositions.indexOf("N") != -1)){
+							quadsAvailible.push('NE'); 
+						}
+						//SW
+						if ((openPositions.indexOf("SW") != -1 && openPositions.indexOf("W") != -1 && openPositions.indexOf("S") != -1)){
+							quadsAvailible.push('SW'); 
+						}
+						//SE
+						if ((openPositions.indexOf("SE") != -1 && openPositions.indexOf("E") != -1 && openPositions.indexOf("S") != -1)){
+							quadsAvailible.push('SE'); 
+						}
+						
+						//check the length of quadsAvailible if it is 1 then that is the direction other wise use random to find.
+						if (quadsAvailible.length == 0) {
+							placementFound = false;
+						}
+						else if (quadsAvailible.length == 1) {
+							//get rotation going N/S or E/W (default is E/W)
+							furnitureRotation = quadsAvailible[0]
+						}
+						else {
+							//pick randomly
+							x = Math.floor(furnitureRandom() * quadsAvailible.length)
+							furnitureRotation = quadsAvailible[x]
+						}
+						if (furnitureRotation != "") {
+							if (furnitureRotation == "SE") {
+								furnitureVariant = Math.floor(furnitureRandom() * furniture2x2Variants) + 1
+								Crafty.e('TileOpen' + row + '_' + col + ', furnitureMap , 1_' + roomColour + '_2x2_' + furnitureVariant)
+									.attr({y: Crafty('Tile' + row + '_' + col)._y, x: Crafty('Tile' + row + '_' + col)._x, w: _tileSize*2, h: _tileSize*2, xTile: col, yTile: row});
+								Crafty(Crafty('FloorGround')[0]).attach(Crafty('TileOpen' + row + '_' + col));
+								//block off tiles
+								objectMap[row][col] = "T";
+								objectMap[row][col+1] = "T";
+								floorMap[row][col+1] = "o";
+								objectMap[row+1][col+1] = "T";
+								floorMap[row+1][col+1] = "o";
+								objectMap[row+1][col] = "T";
+								floorMap[row+1][col] = "o";
+								
+							}
+							else if (furnitureRotation == "SW") {
+								furnitureVariant = Math.floor(furnitureRandom() * furniture2x2Variants) + 1
+								Crafty.e('TileOpen' + row + '_' + col + ', furnitureMap , 1_' + roomColour + '_2x2_' + furnitureVariant)
+									.attr({y: Crafty('Tile' + row + '_' + col)._y, x: Crafty('Tile' + row + '_' + (col-1))._x, w: _tileSize*2, h: _tileSize*2, xTile: col, yTile: row});
+								Crafty(Crafty('FloorGround')[0]).attach(Crafty('TileOpen' + row + '_' + col));
+								Crafty('TileOpen' + row + '_' + col).origin("center");
+								Crafty('TileOpen' + row + '_' + col).rotation = 180;
+								objectMap[row][col] = "T";
+								objectMap[row][col-1] = "T";
+								floorMap[row][col-1] = "o";
+								objectMap[row+1][col-1] = "T";
+								floorMap[row+1][col-1] = "o";
+								objectMap[row+1][col] = "T";
+								floorMap[row+1][col] = "o";
+							}
+							else if (furnitureRotation == "NE") {
+
+								furnitureVariant = Math.floor(furnitureRandom() * furniture2x2Variants) + 1
+								Crafty.e('TileOpen' + row + '_' + col + ', furnitureMap , 1_' + roomColour + '_2x2_' + furnitureVariant)
+									.attr({y: Crafty('Tile' + (row-1) + '_' + col)._y, x: Crafty('Tile' + row + '_' + col)._x, w: _tileSize*2, h: _tileSize+2, xTile: col, yTile: row});
+								Crafty(Crafty('FloorGround')[0]).attach(Crafty('TileOpen' + row + '_' + col));
+								Crafty('TileOpen' + row + '_' + col).origin(_tileSize/2,_tileSize/2);
+								Crafty('TileOpen' + row + '_' + col).rotation = 270;
+								//block off tiles
+								objectMap[row][col] = "T";
+								objectMap[row-1][col] = "T";
+								floorMap[row-1][col] = "o";
+								objectMap[row-1][col+1] = "T";
+								floorMap[row-1][col+1] = "o";
+								objectMap[row][col+1] = "T";
+								floorMap[row][col+1] = "o";
+
+							}
+							else if (furnitureRotation == "NW") {
+								furnitureVariant = Math.floor(furnitureRandom() * furniture2x2Variants) + 1
+								Crafty.e('TileOpen' + row + '_' + col + ', furnitureMap , 1_' + roomColour + '_2x2_' + furnitureVariant)
+									.attr({y: Crafty('Tile' + (row-1) + '_' + col)._y, x: Crafty('Tile' + row + '_' + (col-1))._x, w: _tileSize*2, h: _tileSize*2, xTile: col, yTile: row});
+								Crafty(Crafty('FloorGround')[0]).attach(Crafty('TileOpen' + row + '_' + col));
+								Crafty('TileOpen' + row + '_' + col).origin(_tileSize/2,_tileSize/2);
+								Crafty('TileOpen' + row + '_' + col).rotation = 90;
+								//block off tiles
+								objectMap[row][col] = "T";
+								objectMap[row-1][col-1] = "T";
+								floorMap[row-1][col-1] = "o";
+								objectMap[row-1][col] = "T";
+								floorMap[row-1][col] = "o";
+								objectMap[row][col-1] = "T";
+								floorMap[row][col-1] = "o";
+							}
+						}					
 					}
 
 				}
